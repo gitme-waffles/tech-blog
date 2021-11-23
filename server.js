31f30bbe9dcc611eac76472884b8081c97d85f8f
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-23 14:43:25 +1100

    setup express midleware

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const routes = require("./controllers")
-const express = require("express")
-const session = require("express-session")
-const exphbs = require("express-handlebars")
+// import packages
+const routes = require("./controllers");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 
-const sequelize = require("./config/connection")
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(static.join(__dirname, "public")));
+
+app.use(routes);
